04e29af485874297d357ed701ae67cf6082df6e3
Artem-Evsiukov <maluy.chert@gmail.com>
2020-06-11 10:32:34 +0300

    added scr

---
+++ src/js/plugins/develop.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    //mobile menu
+    $('.mobile-button').on('click', function () {
+        $('.navigation').slideToggle(400);
+    });
+
+    $(window).on('resize', function () {
+        const windowWidth = $(window).outerWidth();
+        if(windowWidth > 768) {
+            $('.navigation').removeAttr('style');
+        }
+    });
+
+    //slider
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 3000,
+        },
+    })
+
+    //modal
+
+    $('.header').on('click', '.login-button', function() {
+       $('#login-modal').fadeIn();
+   }); 
+
+    $('.modal__window').on('click', '.modal__close', function() {
+       $('#login-modal').fadeOut();
+       
+           
+       }); 
+
+ });
